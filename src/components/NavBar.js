8856065836a156304cc4bb541751732d8fd84536
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-05-08 09:35:19 +0300

    Converted arrays into elements and added key attributes to them

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLink = links.map((singleLink) => {
+    return <a key={singleLink} href={`#${singleLink}`}>{singleLink}</a>
+  })
+
+  return (
+    <nav>
+      {/* display an <a> tag for each link here */}
+      {navLink}
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ id, name, about, technologies }) {
+
+  const allProjectTechUsed = technologies.map((singleTechnology, index) => {
+    return <span key={`${id}${index}`}>{singleTechnology}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {allProjectTechUsed}
       </div>
     </div>
   );
